883366eb128f99bd458846f825c03f4dd549fafb
Sanjay Chakrapani <csanjay@gmail.com>
2015-09-11 19:53:07 -0400

    Added schema and updated tests with new schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,82 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+-- ---
+-- Globals
+-- ---
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(512) NULL DEFAULT NULL,
+  `userId` INTEGER NULL DEFAULT NULL,
+  `roomId` INTEGER NULL DEFAULT NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (userId) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (roomId) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`text`,`userId`,`roomId`,`createdAt`) VALUES
+-- ('','','','','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
 
 
 /*  Execute this file from the command line by typing:
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe("Persistent Node Chat Server", function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+       var tablename = "messages"; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -65,22 +65,47 @@ describe("Persistent Node Chat Server", function() {
 
   it("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
-    // TODO - The exact query string and query args to use
-    // here depend on the schema you design, so I'll leave
-    // them up to you. */
-
-    dbConnection.query(queryString, queryArgs, function(err) {
+/*       var queryString = "START TRANSACTION;" +
+                          "INSERT into rooms (name) values ('main');" +
+                          "INSERT into users (name) values ('test');" +
+                          "COMMIT;";
+*/
+    var roomString = "INSERT into rooms (name) values ('main')";
+    var roomArgs = [];
+    dbConnection.query(roomString, roomArgs, function(err) {
       if (err) { throw err; }
+      var userString = "INSERT into users (name) values ('testspec')";
+      var userArgs = [];
+      dbConnection.query(userString, userArgs, function(err) {
+        if (err) { throw err; }
+        var roomIdString = "SELECT id FROM rooms WHERE name='main'";
+        var roomIdArgs = [];
+        dbConnection.query(roomIdString, roomIdArgs, function(err, results) {
+          if (err) { throw err; }
+          var roomId = results[0].id;
+          var userIdString = "SELECT id FROM users WHERE name='testspec'";
+          var userIdArgs = [];
+          dbConnection.query(userIdString, userIdArgs, function(err, results) {
+            if (err) { throw err; }
+            var userId = results[0].id;
+            var messageString = "INSERT into messages (text, roomId, userId) " +
+                                "values ('Men like you can never change!'," + roomId +
+                                "," + userId + ")";
+            var messageArgs = [];
+            dbConnection.query(messageString, messageArgs, function(err) {
+              if (err) { throw err; }
 
-      // Now query the Node chat server and see if it returns
-      // the message we just inserted:
-      request("http://127.0.0.1:3000/classes/messages", function(error, response, body) {
-        var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal("Men like you can never change!");
-        expect(messageLog[0].roomname).to.equal("main");
-        done();
+              // Now query the Node chat server and see if it returns
+              // the message we just inserted:
+              request("http://127.0.0.1:3000/classes/messages", function(error, response, body) {
+                var messageLog = JSON.parse(body);
+                expect(messageLog[0].text).to.equal("Men like you can never change!");
+                expect(messageLog[0].roomname).to.equal("main");
+                done();
+              });
+            });
+          });
+        });
       });
     });
   });
